46d9475d58c2125d62173d4307d201083e0731ca
IDarkgenesis <byidarkgenesis@gmail.com>
2025-01-27 12:22:40 +0100

    Added temporal orthographic camera for quadtree viewer rendering

--- SobrassadaEngine/UI/QaudtreeViewer.cpp
+++ SobrassadaEngine/UI/QaudtreeViewer.cpp
@@ -1,11 +1,11 @@
 #include "QaudtreeViewer.h"
 
 #include "Application.h"
+#include "CameraModule.h"
 #include "Framebuffer.h"
 #include "Globals.h"
-#include "WindowModule.h"
 #include "ShaderModule.h"
-#include "CameraModule.h"
+#include "WindowModule.h"
 
 #include "SDL.h"
 #include "glew.h"
@@ -13,17 +13,34 @@
 
 QaudtreeViewer::QaudtreeViewer()
 {
-    // TODO: USE CAMERA COMPONENT / CLASS TO CREATE A ORTHOGONAL CAMERA FRON RENDERING THE QUADTREE
     program     = App->GetShaderModule()->GetProgram("./Test/basicVertexShader.vs", "./Test/basicFragmentShader.fs");
     framebuffer = new Framebuffer(SCREEN_WIDTH, SCREEN_HEIGHT, true);
-    float vtx_data[] = {
+    // Triangle
+    /*float vtx_data[] = {
         -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-    };
+    };*/
+
+    // Lines
+    float vtx_data[] = {-1.0f, -1.0f, 1.0f, -1.0f};
 
     glGenBuffers(1, &vbo);
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vtx_data), vtx_data, GL_STATIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // TODO: USE CAMERA COMPONENT / CLASS TO CREATE A ORTHOGONAL CAMERA FRON RENDERING THE QUADTREE
+    camera.type               = FrustumType::OrthographicFrustum;
+    camera.pos                = float3(0, 0, 5);
+    camera.front              = -float3::unitZ;
+    camera.up                 = float3::unitY;
+    camera.nearPlaneDistance  = 0.1f;
+    camera.farPlaneDistance   = 100.f;
+
+    camera.orthographicHeight = (float)framebuffer->GetTextureHeight() / cameraSizeScaleFactor;
+    camera.orthographicWidth  = (float)framebuffer->GetTextureWidth() / cameraSizeScaleFactor;
+
+    viewMatrix                = camera.ViewMatrix();
+    projectionMatrix          = camera.ProjectionMatrix();
 }
 
 QaudtreeViewer::~QaudtreeViewer()
@@ -39,24 +56,29 @@ void QaudtreeViewer::Render(bool &renderBoolean)
 
     // Clear current viewport
     glViewport(0, 0, framebuffer->GetTextureWidth(), framebuffer->GetTextureHeight());
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    float4x4 model, view, proj;
-    proj  = App->GetCameraModule()->GetProjectionMatrix();
-    view  = App->GetCameraModule()->GetViewMatrix();
+    float4x4 model;
     model = float4x4::identity;
 
     glUseProgram(program);
-    glUniformMatrix4fv(0, 1, GL_TRUE, &proj[0][0]);
-    glUniformMatrix4fv(1, 1, GL_TRUE, &view[0][0]);
+    glUniformMatrix4fv(0, 1, GL_TRUE, &projectionMatrix[0][0]);
+    glUniformMatrix4fv(1, 1, GL_TRUE, &viewMatrix[0][0]);
     glUniformMatrix4fv(2, 1, GL_TRUE, &model[0][0]);
 
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    // Triangle
+    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
+
+    // Lines
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void *)0);
+
+    // Triangle
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_LINES, 0, 2);
 
     // Unbind everything to not mess with other renders
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -85,10 +107,19 @@ void QaudtreeViewer::Render(bool &renderBoolean)
             ImVec2 windowSize = ImGui::GetWindowSize();
             if (framebuffer->GetTextureWidth() != windowSize.x || framebuffer->GetTextureHeight() != windowSize.y)
             {
-                float aspectRatio = windowSize.y / windowSize.x;
+                ChangeCameraSize(windowSize.x, windowSize.y);
                 framebuffer->Resize((int)windowSize.x, (int)windowSize.y);
             }
         }
     }
     ImGui::End();
+}
+
+void QaudtreeViewer::ChangeCameraSize(float width, float height) 
+{
+    camera.orthographicHeight = width / cameraSizeScaleFactor;
+    camera.orthographicWidth  = height / cameraSizeScaleFactor;
+
+    viewMatrix                = camera.ViewMatrix();
+    projectionMatrix          = camera.ProjectionMatrix();
 }
--- SobrassadaEngine/UI/QaudtreeViewer.h
+++ SobrassadaEngine/UI/QaudtreeViewer.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "Geometry/Frustum.h"
+#include "Math/float4x4.h"
+
 class Framebuffer;
 
 class QaudtreeViewer
@@ -8,10 +11,18 @@ class QaudtreeViewer
     QaudtreeViewer();
     ~QaudtreeViewer();
 
-    void Render(bool& renderBoolean);
+    void Render(bool &renderBoolean);
+
+  private:
+    void ChangeCameraSize(float width, float height);
 
   private:
     Framebuffer *framebuffer;
-    unsigned int vbo = 0;
-    unsigned int program = 0;
+    unsigned int vbo            = 0;
+    unsigned int program        = 0;
+    float cameraSizeScaleFactor = 10.f;
+
+    Frustum camera;
+    float4x4 viewMatrix;
+    float4x4 projectionMatrix;
 };
