3e71dc6ba6d7f5c2a14d3d073ae31805634fed5d
Marc Casanova <mcasanto14@alumnes.ub.edu>
2025-02-14 18:02:45 +0100

    filter and files moved to folders

--- SobrassadaEngine/Utils/log.cpp
+++ SobrassadaEngine/Utils/log.cpp
@@ -1,26 +1,29 @@
 #pragma once
 #include "Globals.h"
 
-void glog(const char file[], int line, const char* format, ...)
+void glog(const char file[], int line, const char *format, ...)
 {
-	static char tmp_string[4096];
-	static char tmp_string2[4096];
-	static va_list  ap;
+    static char tmp_string[4096];
+    static char tmp_string2[4096];
+    static va_list ap;
 
-	// Construct the string from variable arguments
-	va_start(ap, format);
-	vsprintf_s(tmp_string, 4096, format, ap);
-	va_end(ap);
-	sprintf_s(tmp_string2, 4096, "\n%s(%d) : %s", file, line, tmp_string);
-	OutputDebugString(tmp_string2);
+    // Construct the string from variable arguments
+    va_start(ap, format);
+    vsprintf_s(tmp_string, 4096, format, ap);
+    va_end(ap);
+    sprintf_s(tmp_string2, 4096, "\n%s(%d) : %s", file, line, tmp_string);
 
-	// Allocating memory to store logs
-	char* newString;
-	newString = (char*)malloc(4096);
+#ifdef _DEBUG
+    OutputDebugString(tmp_string2);
+#endif
 
-	if (newString == NULL) return;
+    // Allocating memory to store logs
+    char *newString;
+    newString = (char *)malloc(4096);
 
-	memcpy(newString, tmp_string, 4096 * sizeof(char));
+    if (newString == NULL) return;
 
-	Logs->push_back(newString);
+    memcpy(newString, tmp_string, 4096 * sizeof(char));
+
+    Logs->push_back(newString);
 }
