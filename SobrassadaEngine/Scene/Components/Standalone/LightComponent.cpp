e90e361b80e5143743c63566f0a3dabbd2062623
PolCurto <115176650+PolCurto@users.noreply.github.com>
2025-02-23 18:21:25 +0100

    Add spacing to UI

--- SobrassadaEngine/Scene/Components/Standalone/LightComponent.cpp
+++ SobrassadaEngine/Scene/Components/Standalone/LightComponent.cpp
@@ -60,6 +60,7 @@ void LightComponent::RenderEditorInspector()
         ImGui::SliderFloat3("Color", &color[0], 0.0f, 1.0f);
         ImGui::SliderFloat("Intensity", &intensity, 0.0f, 100.0f);
         ImGui::Checkbox("Draw gizmos", &drawGizmos);
+        ImGui::Spacing();
     }
 }
 
