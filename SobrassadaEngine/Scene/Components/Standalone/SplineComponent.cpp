a08efb16306da9faa132c119a87d47df6ba49675
Marc López <marclop.se@gmail.com>
2025-05-15 13:13:46 +0200

    clone implemented

--- SobrassadaEngine/Scene/Components/Standalone/SplineComponent.cpp
+++ SobrassadaEngine/Scene/Components/Standalone/SplineComponent.cpp
@@ -1,6 +1,7 @@
 #include "SplineComponent.h"
 
 #include "GameObject.h"
+#include "imgui.h"
 
 SplineComponent::SplineComponent(UID uid, GameObject* parent) : Component(uid, parent, "Spline", COMPONENT_SPLINE)
 {
@@ -37,14 +38,42 @@ void SplineComponent::RenderDebug(float deltaTime)
 
 void SplineComponent::RenderEditorInspector()
 {
+    Component::RenderEditorInspector();
+
+    if(ImGui::TreeNode("Spline"))
+    {
+        ImGui::Text("Points: %zu", points.size());
+        ImGui::DragFloat("Tension", &tension, 0.01f, 0.0f, 1.0f);
+        
+        if (ImGui::Button("Add Point")) points.push_back(float3::zero);
+
+        ImGui::TreePop();
+    }
 }
 
-void SplineComponent::Save(rapidjson::Value&, rapidjson::Document::AllocatorType&) const
+void SplineComponent::Save(rapidjson::Value& targetState, rapidjson::Document::AllocatorType& allocator) const
 {
+    Component::Save(targetState, allocator);
+    targetState.AddMember("Tension", tension, allocator);
+    rapidjson::Value arr(rapidjson::kArrayType);
+
+    for (const auto& p : points)
+    {
+        rapidjson::Value pArr(rapidjson::kArrayType);
+        pArr.PushBack(p.x, allocator).PushBack(p.y, allocator).PushBack(p.z, allocator);
+        arr.PushBack(pArr, allocator);
+    }
+
+    targetState.AddMember("Points", arr, allocator);
 }
 
 void SplineComponent::Clone(const Component* other)
 {
+    if (other->GetType() != COMPONENT_SPLINE) return;
+
+    const SplineComponent* otherSpline = static_cast<const SplineComponent*>(other);
+    points                             = otherSpline->points;
+    tension                            = otherSpline->tension;
 }
 
 void SplineComponent::AddPoint(const float3& p)
--- SobrassadaEngine/Scene/Components/Standalone/SplineComponent.h
+++ SobrassadaEngine/Scene/Components/Standalone/SplineComponent.h
@@ -17,7 +17,7 @@ public:
     void Render(float deltaTime) override;
     void RenderDebug(float deltaTime) override;
     void RenderEditorInspector() override;
-    void Save(rapidjson::Value&, rapidjson::Document::AllocatorType&) const override;
+    void Save(rapidjson::Value& targetState, rapidjson::Document::AllocatorType& allocator) const override;
     void Clone(const Component* other) override;
 
     void AddPoint(const float3& p);
