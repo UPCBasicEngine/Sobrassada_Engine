91d7f52721341ee67f0d1d1d79e29ec29801254b
Joan Llobet <j.llobetruiz@gmail.com>
2025-05-17 03:11:19 +0200

    quaternion interpolation done, now the tiles properly come and go to specified transform

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/TileFloatScript.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/TileFloatScript.cpp
@@ -14,7 +14,8 @@
 
 TileFloatScript::TileFloatScript(GameObject* parent) : Script(parent)
 {
-    fields.push_back({"Speed", InspectorField::FieldType::Float, &speed, 0.0f, 100.0f});
+    fields.push_back({"Speed", InspectorField::FieldType::Int, &speed, 0, 100});
+    fields.push_back({"MinDistanceToPlayer", InspectorField::FieldType::Float, &minDistanceToPlayer, 0.0f, 100.0f});
     fields.push_back({"Starting Position", InspectorField::FieldType::Vec3, &startPosition, 0.0, 100.0f});
     fields.push_back({"Starting Rotation", InspectorField::FieldType::Vec3, &startRotation, 0.0, 100.0f});
     fields.push_back({"Starting Scale", InspectorField::FieldType::Vec3, &startScale, 0.0, 100.0f});
@@ -23,6 +24,7 @@ TileFloatScript::TileFloatScript(GameObject* parent) : Script(parent)
 void TileFloatScript::Save(rapidjson::Value& targetState, rapidjson::Document::AllocatorType& allocator)
 {
     targetState.AddMember("Speed", speed, allocator);
+    targetState.AddMember("MinDistanceToPlayer", minDistanceToPlayer, allocator);
 
     rapidjson::Value startPos(rapidjson::kArrayType);
     startPos.PushBack(startPosition.x, allocator);
@@ -45,7 +47,10 @@ void TileFloatScript::Save(rapidjson::Value& targetState, rapidjson::Document::A
 
 void TileFloatScript::Load(const rapidjson::Value& initialState)
 {
-    if (initialState.HasMember("Speed") && initialState["Speed"].IsFloat()) speed = initialState["Speed"].GetFloat();
+    if (initialState.HasMember("Speed") && initialState["Speed"].IsInt()) speed = initialState["Speed"].GetInt();
+
+    if (initialState.HasMember("MinDistanceToPlayer") && initialState["MinDistanceToPlayer"].IsFloat())
+        minDistanceToPlayer = initialState["MinDistanceToPlayer"].GetFloat();
 
     if (initialState.HasMember("StartPosition") && initialState["StartPosition"].IsArray())
     {
@@ -68,23 +73,19 @@ void TileFloatScript::Load(const rapidjson::Value& initialState)
 
 bool TileFloatScript::Init()
 {
-    character->GetLastPosition();
-    if (!character)
-    {
-        GLOG("CharacterController component not found for CuChulainn");
-        return false;
-    }
 
     // get final (correct) position, and move the tile to start (rotated, moved and scaled) position
-    finalPosition           = parent->GetLocalTransform().TranslatePart();
-    finalRotation           = Quat(parent->GetLocalTransform().RotatePart());
-    finalScale              = parent->GetLocalTransform().GetScale();
+    float4x4 originalTransform = parent->GetLocalTransform();
+    finalPosition           = originalTransform.TranslatePart();
+    finalRotation           = Quat(originalTransform.RotatePart());
+    finalScale              = originalTransform.GetScale();
 
-    Quat startQuat          = Quat::FromEulerXYZ(startRotation.x, startRotation.y, startRotation.z);
+    startQuat          = Quat::FromEulerXYZ(startRotation.x, startRotation.y, startRotation.z);
 
     float4x4 startTransform = float4x4::FromTRS(startPosition, startQuat, startScale);
 
     parent->SetLocalTransform(startTransform);
+    parent->UpdateTransformForGOBranch();
 
     GLOG("Initiating TileFloatScript");
     return true;
@@ -94,16 +95,16 @@ void TileFloatScript::Update(float deltaTime)
 {
     if (!character) return;
 
-    float distance = character->GetLastPosition().Distance(parent->GetPosition());
+    float distance = character->GetLastPosition().Distance(finalPosition);
 
-    // Set active when in range
-    isActive       = (distance <= 10.0f); // Adjust this threshold as needed
+    // Disable when far away (for performance)
+    isActive       = (distance <= minDistanceToPlayer * 2);
     if (!isActive) return;
 
-    bool goingToFinal   = distance <= 6.0f;
+    bool goingToFinal   = distance <= minDistanceToPlayer;
 
-    float factor        = 1.0f / max(distance, 0.1f);
-    float adjustedSpeed = speed * deltaTime * factor;
+    float factor        = max(distance, 0.1f);
+    float adjustedSpeed = speed * deltaTime *(1.0f / factor);
 
     float3 currentT     = parent->GetPosition();
     float3 currentS     = parent->GetScale();
@@ -112,11 +113,17 @@ void TileFloatScript::Update(float deltaTime)
 
     float3 targetT = goingToFinal ? finalPosition : startPosition;
     float3 targetS = goingToFinal ? finalScale : startScale;
-    Quat targetQuat =
-        goingToFinal ? finalRotation : Quat::FromEulerXYZ(startRotation.x, startRotation.y, startRotation.z);
+
+    Quat targetQuat       = goingToFinal ? finalRotation : startQuat;
 
     float3 newT           = Lerp(currentT, targetT, adjustedSpeed);
     float3 newS           = Lerp(currentS, targetS, adjustedSpeed);
+
+    if (QuaternionDot(currentQuat, targetQuat) < 0.0f)
+    {
+        targetQuat = Quat(-targetQuat.x, -targetQuat.y, -targetQuat.z, -targetQuat.w); 
+    }
+
     Quat newQuat          = Quat::Slerp(currentQuat, targetQuat, adjustedSpeed);
 
     float4x4 newTransform = float4x4::FromTRS(newT, newQuat, newS);
--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/TileFloatScript.h
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/TileFloatScript.h
@@ -11,14 +11,16 @@ class TileFloatScript : public Script
     void Update(float deltaTime) override;
     void Save(rapidjson::Value& targetState, rapidjson::Document::AllocatorType& allocator) override;
     void Load(const rapidjson::Value& initialState) override;
-
+    inline float QuaternionDot(const Quat& a, const Quat& b) { return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w; }
   private:
-    float speed = 50.0f;
+    int speed = 10;
     float3 finalPosition;
     Quat finalRotation;
+    Quat startQuat;
     float3 finalScale;
     float3 startPosition;
     float3 startRotation;
-    float3 startScale;
+    float3 startScale = float3(1.0f,1.0f,1.0f);
+    float minDistanceToPlayer = 10.0f;
     bool isActive = false;
 };
