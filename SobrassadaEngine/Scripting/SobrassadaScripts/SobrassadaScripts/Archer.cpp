a2d3478c0a21c8e14822936c65bff596a1e2f5f1
Pablo García Fuentes <115176432+PabioGF@users.noreply.github.com>
2025-05-19 17:27:36 +0200

    First commit-THO-571

---
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Archer.cpp
@@ -0,0 +1,157 @@
+#include "pch.h"
+
+#include "Application.h"
+#include "Component.h"
+#include "CuChulainn.h"
+#include "GameObject.h"
+#include "GameTimer.h"
+#include "Globals.h"
+#include "ResourceStateMachine.h"
+#include "Archer.h"
+#include "Standalone/AIAgentComponent.h"
+#include "Standalone/AnimationComponent.h"
+#include "Standalone/CharacterControllerComponent.h"
+#include "Standalone/Physics/CapsuleColliderComponent.h"
+
+Archer::Archer(GameObject* parent) : Character(parent, 3, 1, 0.5f, 1.0f, 1.0f, 2.0f, 10.0f, CharacterType::Archer)
+{
+    fields.push_back({"AI Patrol Point", InspectorField::FieldType::Vec3, &patrolPoint});
+}
+
+bool Archer::Init()
+{
+    // GLOG("Initiating Soldier");
+
+    currentState = ArcherStates::PATROL;
+
+    Character::Init();
+
+    agentAI = parent->GetComponent<AIAgentComponent*>();
+    if (agentAI == nullptr) GLOG("AIAgent component not found for Archer")
+    else
+    {
+        agentAI->RecreateAgent();
+        agentAI->SetLookForward(true);
+        speed = agentAI->GetSpeed();
+    }
+
+    return true;
+}
+
+void Archer::Update(float deltaTime)
+{
+    if (agentAI == nullptr) return;
+    Character::Update(deltaTime);
+}
+
+void Archer::OnDeath()
+{
+    // TODO: include death sound for the character
+    // TODO: animation and particles
+}
+
+void Archer::OnDamageTaken(int amount)
+{
+    // TODO: play soldier take damage sound
+    // TODO: particles? and animation
+}
+
+void Archer::PerformAttack()
+{
+    // TODO: play basicAttack sound
+    // TODO: make interaction with hitboxes with the character
+    // TODO: activate and disable the box collider located on one on the gameobjects weapon
+    // TODO: trails, particles and animation
+}
+
+void Archer::HandleState(float deltaTime)
+{
+    // if (!animComponent) return;
+
+    switch (currentState)
+    {
+    case ArcherStates::PATROL:
+        // GLOG("Soldier Patrolling");
+        // animComponent->UseTrigger("idle");
+        PatrolAI();
+        break;
+    case ArcherStates::CHASE:
+        // GLOG("Soldier Chasing");
+        //  animComponent->UseTrigger("Run");
+        ChaseAI();
+        break;
+    case ArcherStates::BASIC_ATTACK:
+        // GLOG("Soldier Basic Attack");
+        //  animComponent->UseTrigger("attack");
+        if (attackCdTimer <= 0) Attack(deltaTime);
+        break;
+    default:
+        GLOG("No state provided to Archer");
+        currentState = ArcherStates::PATROL;
+        break;
+    }
+}
+
+void Archer::PatrolAI()
+{
+    if (CheckDistanceWithPlayer() == PlayerDistances::Medium) currentState = ArcherStates::CHASE;
+    else if (CheckDistanceWithPlayer() == PlayerDistances::Close) currentState = ArcherStates::BASIC_ATTACK;
+
+    bool valid = false;
+    if (reachedPatrolPoint)
+    {
+        if (CheckDistanceWithPoint(startPos)) reachedPatrolPoint = false;
+        else valid = agentAI->SetPathNavigation(startPos);
+    }
+    else
+    {
+        if (CheckDistanceWithPoint(patrolPoint)) reachedPatrolPoint = true;
+        else valid = agentAI->SetPathNavigation(patrolPoint);
+    }
+}
+
+void Archer::ChaseAI()
+{
+    if (character != nullptr)
+    {
+        if (CheckDistanceWithPlayer() == PlayerDistances::Close) currentState = ArcherStates::BASIC_ATTACK;
+        else if (!agentAI->SetPathNavigation(character->GetLastPosition())) currentState = ArcherStates::PATROL;
+    }
+    else currentState = ArcherStates::PATROL;
+}
+
+void Archer::Attack(float deltaTime)
+{
+    if (!weaponCollider) return;
+
+    if (!isAttacking)
+    {
+        GLOG("ATTACK ENEMY");
+        if (animComponent) animComponent->UseTrigger("Attack");
+        Character::Attack(deltaTime);
+        agentAI->PauseMovement();
+    }
+    else
+    {
+
+        // Enable hitbox when animation hits
+        if (!weaponCollider->GetEnabled() && attackTimer >= attackHitboxDelay &&
+            attackTimer <= attackHitboxDelay + attackHitboxDuration)
+        {
+            weaponCollider->SetEnabled(true);
+        }
+        else if (weaponCollider->GetEnabled() && attackTimer >= attackHitboxDelay + attackHitboxDuration)
+        {
+            weaponCollider->SetEnabled(false);
+        }
+
+        // Reset attack state
+        if (attackTimer >= attackDuration)
+        {
+            isAttacking   = false;
+            attackCdTimer = attackCooldown;
+            agentAI->ResumeMovement();
+            if (CheckDistanceWithPlayer() != PlayerDistances::Close) currentState = ArcherStates::CHASE;
+        }
+    }
+}
---
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Archer.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Character.h"
+
+class GameObject;
+class AIAgentComponent;
+
+enum class ArcherStates
+{
+    NONE,
+    PATROL,
+    CHASE,
+    BASIC_ATTACK
+};
+
+class Archer : public Character
+{
+  public:
+    Archer(GameObject* parent);
+    ~Archer() noexcept override { parent = nullptr; };
+
+    bool Init() override;
+    void Update(float deltaTime) override;
+
+  private:
+    void OnDeath() override;
+    void OnDamageTaken(int amount) override;
+    void PerformAttack() override;
+    void HandleState(float deltaTime) override;
+    void Attack(float deltaTime) override;
+
+    void PatrolAI();
+    void ChaseAI();
+
+  private:
+    AIAgentComponent* agentAI  = nullptr;
+    ArcherStates currentState  = ArcherStates::NONE;
+
+    float3 patrolPoint         = float3::zero;
+};
