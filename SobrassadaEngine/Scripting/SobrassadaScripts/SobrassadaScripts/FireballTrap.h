9a163dfe7dd1acf78ec41b789f300d871d575b20
Marc Casanova <55861084+SirKaza@users.noreply.github.com>
2025-05-20 00:56:51 +0200

    Task/Fireball Trap THO-500 (#191)
    * trap done
    
    * mesh ground only on impact and more variables in script inspector
    
    * new filters & small fix
    
    * trying to fix bug
    
    * WIP: bug fix
    
    * visual bug only in debug

---
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/FireballTrap.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Script.h"
+
+class GameObject;
+class MeshComponent;
+class SphereColliderComponent;
+
+class FireballTrap : public Script
+{
+  public:
+    FireballTrap(GameObject* parent);
+    bool Init() override;
+    void Update(float deltaTime) override;
+
+    int GetDamage() const { return damage; }
+
+  private:
+    void StartAttack(float gameTime);
+    void HandleImpact(float gameTime);
+    void DisableDamage();
+    void UpdateFireball(float deltaTime);
+
+  private:
+    bool activated                          = false;
+    float activationRange                   = 10.0f;
+    float attackCooldown                    = 5.0f;
+    int damage                              = 1;
+    float damageDuration                    = 1.5f;
+    bool attacking                          = false;
+
+    MeshComponent* groundMesh                 = nullptr;
+    SphereColliderComponent* damageCollider = nullptr;
+
+    float lastAttackTime                    = -1.0f;
+    float lastHitTime                       = -1.0f;
+    bool isDealingDamage                    = false;
+
+    // fireball
+    std::string fireballName                = "";
+    GameObject* fireball                    = nullptr;
+    float verticalSpeed                     = 0.0f;
+    float gravity                           = -9.81f;
+    float maxFallSpeed                      = -20.0f;
+    bool hasImpacted                        = false;
+
+    float rotationSpeed                     = 1.0f;
+    float fallingHeight                     = 20.0f;
+    float editableMaxFallSpeed              = 20.0f;
+};
