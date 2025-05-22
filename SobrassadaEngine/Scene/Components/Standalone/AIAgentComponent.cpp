cce9bcf1c0cd49171617064456d8f7e8c42b0804
Pablo García Fuentes <115176432+PabioGF@users.noreply.github.com>
2025-05-22 19:19:13 +0200

    Delete Glogs-THO-571

--- SobrassadaEngine/Scene/Components/Standalone/AIAgentComponent.cpp
+++ SobrassadaEngine/Scene/Components/Standalone/AIAgentComponent.cpp
@@ -107,7 +107,6 @@ void AIAgentComponent::Update(float deltaTime)
         }
     }
     else newPos = float3(ag->npos[0], ag->npos[1], ag->npos[2]);
-    GLOG("new pos: %f %f %f", newPos.x, newPos.y, newPos.z);
 
     // float4x4 transform = parent->GetLocalTransform();
     parent->SetLocalPosition(newPos - parent->GetParentGlobalTransform().TranslatePart()); // Change parent position
--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Archer.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Archer.cpp
@@ -150,11 +150,6 @@ void Archer::Attack(float deltaTime)
         {
             float3 direction = character->GetLastPosition() - parent->GetGlobalTransform().TranslatePart();
             direction.Normalize();
-
-            GLOG(
-                "Parent name: %s, and position:(%f,%f,%f)", parent->GetName().c_str(), parent->GetPosition().x,
-                parent->GetPosition().y, parent->GetPosition().z
-            )
             arrow->Shoot(parent->GetPosition(), direction);
         }
 
