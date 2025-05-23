0c8b0b49eeeb6d9245182ac95f5122bd5084867c
Joan Llobet <j.llobetruiz@gmail.com>
2025-05-23 11:55:00 +0200

    added one sound for testing

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Wwise_IDs.h
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Wwise_IDs.h
@@ -14,6 +14,7 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID BUCKET = 902301855U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
         static const AkUniqueID ICE_BLAST = 3920196677U;
         static const AkUniqueID LOWDRUM = 2306510041U;
         static const AkUniqueID MUSIC = 3991942870U;
