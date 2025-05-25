86ac7ff562451c3bf7bc3669ad275d06c395ad6d
Marc Casanova <55861084+SirKaza@users.noreply.github.com>
2025-05-26 00:35:06 +0200

    comma missing scriptexport (#231)

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/ScriptExport.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/ScriptExport.cpp
@@ -4,6 +4,7 @@
 #include "Banshee.h"
 #include "ButtonScript.h"
 #include "CameraMovement.h"
+#include "ChangeSceneScript.h"
 #include "CuChulainn.h"
 #include "ExitGameScript.h"
 #include "FireballTrap.h"
@@ -12,6 +13,7 @@
 #include "Globals.h"
 #include "GodMode.h"
 #include "MainMenuSelectorScript.h"
+#include "MenuChangeSceneScript.h"
 #include "OptionsMenuSwitcherScript.h"
 #include "PauseMenuScript.h"
 #include "PressAnyKeyScript.h"
@@ -19,11 +21,9 @@
 #include "RotateGameObject.h"
 #include "Soldier.h"
 #include "SpawnPoint.h"
-#include "ChangeSceneScript.h"
 #include "SpawnUI.h"
 #include "TileFloatScript.h"
 #include "VSyncToggleScript.h"
-#include "MenuChangeSceneScript.h"
 #include <string>
 
 #ifndef SOBRASSADASCRIPTS_EXPORTS
@@ -33,28 +33,28 @@
 #endif
 
 constexpr const char* scripts[] = {
-    "RotateGameObjectScript",   
-    "ButtonScript",              
-    "GodModeScript",             
-    "CuChulainnScript",         
-    "SoldierScript",             
-    "ExitGameScript",            
-    "FullscreenToggleScript",    
-    "VSyncToggleScript",         
-    "PauseMenuScript",          
-    "OptionsMenuSwitcherScript", 
-    "MainMenuSelectorScript",    
-    "PressAnyKeyScript",        
-    "CameraMovement",            
-    "Projectile",                
-    "FreeCamera",                
+    "RotateGameObjectScript",
+    "ButtonScript",
+    "GodModeScript",
+    "CuChulainnScript",
+    "SoldierScript",
+    "ExitGameScript",
+    "FullscreenToggleScript",
+    "VSyncToggleScript",
+    "PauseMenuScript",
+    "OptionsMenuSwitcherScript",
+    "MainMenuSelectorScript",
+    "PressAnyKeyScript",
+    "CameraMovement",
+    "Projectile",
+    "FreeCamera",
     "SpawnPoint",
     "Banshee",
     "TileFloatScript",
     "FireballTrap",
-    "Archer",             
+    "Archer",
     "ChangeSceneScript",
-    "SpawnUI"
+    "SpawnUI",
     "MenuChangeSceneScript"
 };
 
@@ -97,7 +97,6 @@ extern "C" SOBRASSADA_API Script* CreateScript(const std::string& scriptType, Ga
     if (scriptType == "GodModeScript") return new GodMode(parent);
     if (scriptType == "ChangeSceneScript") return new ChangeSceneScript(parent);
     if (scriptType == "FreeCamera") return new FreeCamera(parent);
-
 
     return nullptr;
 }
