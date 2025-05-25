179e55775948f3bb04b7964b318532f8281cb205
Pablo García Fuentes <115176432+PabioGF@users.noreply.github.com>
2025-05-25 10:37:21 +0200

    Task/SoldierV2-THO-492 (#217)
    * Animations changes-THO-492
    
    * Merge with vs3-THO-492
    
    ---------
    
    Co-authored-by: Joan Llobet <j.llobetruiz@gmail.com>

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Soldier.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Soldier.cpp
@@ -66,29 +66,33 @@ void Soldier::PerformAttack()
 
 void Soldier::HandleState(float deltaTime)
 {
-    // if (!animComponent) return;
+    if (!animComponent) return;
 
     switch (currentState)
     {
     case SoldierStates::PATROL:
         // GLOG("Soldier Patrolling");
-        // animComponent->UseTrigger("idle");
         PatrolAI();
+        animComponent->UseTrigger("run");
         break;
     case SoldierStates::CHASE:
         // GLOG("Soldier Chasing");
-        //  animComponent->UseTrigger("Run");
+        animComponent->UseTrigger("run");
         ChaseAI();
         break;
     case SoldierStates::BASIC_ATTACK:
         // GLOG("Soldier Basic Attack");
-        //  animComponent->UseTrigger("attack");
         if (attackCdTimer <= 0) Attack(deltaTime);
         break;
     default:
         GLOG("No state provided to Soldier");
         currentState = SoldierStates::PATROL;
         break;
+    }
+
+    if (animComponent && animComponent->IsFinished())
+    {
+        animComponent->UseTrigger("idle");
     }
 }
 
@@ -127,7 +131,7 @@ void Soldier::Attack(float deltaTime)
     if (!isAttacking)
     {
         GLOG("ATTACK ENEMY");
-        if (animComponent) animComponent->UseTrigger("Attack");
+        if (animComponent) animComponent->UseTrigger("attack");
         Character::Attack(deltaTime);
         agentAI->PauseMovement();
     }
