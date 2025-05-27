ef0d5176ea50cef89e1cf3519a6dbca0c37ac4db
Bartomeu Perelló <44304824+IDarkgenesis@users.noreply.github.com>
2025-05-27 12:57:26 +0200

    Fixed color flickering when using modifed ImGradient (#238)
    * Fixed color flickering when using modifed ImGradient
    
    * Changed curves to work in multiple incles, Andreu's cooking.

--- SobrassadaEngine/Libs/ImGui/imgui_color_gradient.cpp
+++ SobrassadaEngine/Libs/ImGui/imgui_color_gradient.cpp
@@ -50,7 +50,7 @@ void ImGradient::getColorAt(float position, float* color) const
 {
     position      = ImClamp(position, 0.0f, 1.0f);
     int cachePos  = (int)(position * 255);
-    cachePos     *= 3;
+    cachePos     *= 4;
     color[0]      = m_cachedValues[cachePos + 0];
     color[1]      = m_cachedValues[cachePos + 1];
     color[2]      = m_cachedValues[cachePos + 2];
