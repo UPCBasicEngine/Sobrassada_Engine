fa6f15e400c8db5ab181709804e0f8dab54ec7cf
Joan Llobet <j.llobetruiz@gmail.com>
2025-07-08 03:36:54 +0200

    mirage base

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/BossMirage.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/BossMirage.cpp
@@ -1,2 +1,13 @@
 #include "pch.h"
 #include "BossMirage.h"
+
+bool BossMirage::Init()
+{
+    return false;
+}
+
+void BossMirage::Update(float deltaTime)
+{
+
+}
+
--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/BossMirage.h
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/BossMirage.h
@@ -1,4 +1,28 @@
 #pragma once
-class BossMirage
+
+#include "Script.h"
+
+class Mirage;
+
+struct AttackSequence
 {
+    std::vector<Mirage*> mirageZones;
+    float delayBetweenZones = 1.0f;
+};
+
+
+struct SequenceTrigger
+{
+    float hpThreshold;
+    AttackSequence sequence;
+};
+
+class BossMirage : public Script
+{
+    bool Init() override;
+    void Update(float deltaTime) override;
+
+
+  private:
+    int currentPhase = 0;
 };
