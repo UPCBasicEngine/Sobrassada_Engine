f23bf44566f0fecd3976fa6010f563602959a6f4
Joan Llobet <j.llobetruiz@gmail.com>
2025-07-10 15:15:43 +0200

    mirages setup

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Mirage.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Mirage.cpp
@@ -1,2 +1,21 @@
 #include "pch.h"
 #include "Mirage.h"
+
+
+
+Mirage::Mirage(GameObject* parent) : Script(parent)
+{
+    //fields.push_back({"Shape", InspectorField::FieldType::Enum, &ShapeType, 0, 1});
+    fields.push_back({"Delay Before Damage", InspectorField::FieldType::Float, &warningDelay, 0.0f, 10.0f});
+    fields.push_back({"Damage Duration", InspectorField::FieldType::Float, &damageDuration, 0.0f, 10.0f});
+    fields.push_back({"Damage", InspectorField::FieldType::Int, &damage, 0, 100});
+    fields.push_back({"Size X/Radius", InspectorField::FieldType::Float, &sizeX, 0.1f, 50.0f});
+    fields.push_back({"Size Y", InspectorField::FieldType::Float, &sizeY, 0.1f, 50.0f});
+    fields.push_back({"Rotation Y", InspectorField::FieldType::Float, &rotation, -180.0f, 180.0f});
+}
+
+
+void Mirage::Update(float deltaTime)
+{
+
+}
--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Mirage.h
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Mirage.h
@@ -1,4 +1,34 @@
 #pragma once
-class Mirage
+
+#include "Script.h"
+
+enum ACTIVATION_STATE
 {
+    SLEEPING,
+    WARNING,
+    DAMAGING,
+};
+
+enum ShapeType
+{
+    Circle,
+    Rectangle
+};
+
+class Mirage : public Script
+{
+    void Update(float deltaTime) override;
+    Mirage(GameObject* parent);
+
+  protected:
+    UID mirageWarningImage = 0;
+    UID mirageDamageImage  = 0;
+
+    int damage             = 0;
+    float warningDelay     = 0.f;
+    float damageDuration   = 0.f;
+
+    float sizeX            = 2.0f;
+    float sizeY            = 2.0f;
+    float rotation         = 0.0f;
 };
