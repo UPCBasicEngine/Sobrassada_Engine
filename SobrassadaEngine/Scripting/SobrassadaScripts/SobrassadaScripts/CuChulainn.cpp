f61b32bc66ccd032a2e40a56cd3835122501c069
Marc López <marclop.se@gmail.com>
2025-08-04 16:03:49 +0200

    Limit unlocking dash
    Now dash can only be activated only if player has crossed the dash tutorial.

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/CuChulainn.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/CuChulainn.cpp
@@ -467,7 +467,7 @@ void CuChulainn::GetInputs()
 
 bool CuChulainn::CanDash() const
 {
-    //if (!dashUnlocked) return false; //When tutorial map is correctly fixed, put this to make progression
+    if (!dashUnlocked) return false; //When tutorial map is correctly fixed, put this to make progression
 
     bool canDash = dashTimer <= 0 && state != CharacterStates::AIM && !isAttacking && state != CharacterStates::FALL &&
                    state != CharacterStates::RESPAWN && state != CharacterStates::ULTIMATE &&
