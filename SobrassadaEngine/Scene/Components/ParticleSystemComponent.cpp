ecb6420841b13b1a96f037088e9d2e4dcd6375ef
PolCurto <115176650+PolCurto@users.noreply.github.com>
2025-08-13 15:38:07 +0200

    Disable particles when disabled gameObject

--- SobrassadaEngine/Scene/Components/ParticleSystemComponent.cpp
+++ SobrassadaEngine/Scene/Components/ParticleSystemComponent.cpp
@@ -55,8 +55,12 @@ void ParticleSystemComponent::Clone(const Component* other)
 
 void ParticleSystemComponent::Update(float deltaTime)
 {
-    for (auto& emitter : emitterInstances)
-        emitter.Update(deltaTime);
+    if (IsEffectivelyEnabled())
+    {
+        for (auto& emitter : emitterInstances)
+            emitter.Update(deltaTime);
+    }
+   
 }
 
 void ParticleSystemComponent::RenderDebug(float deltaTime)
