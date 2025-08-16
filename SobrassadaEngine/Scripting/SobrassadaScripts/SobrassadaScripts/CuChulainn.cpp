e0cfcc4cfef4f47d019e054bdd27cea8bdf8d03a
PolCurto <115176650+PolCurto@users.noreply.github.com>
2025-08-16 20:11:26 +0200

    Code cleaning

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/CuChulainn.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/CuChulainn.cpp
@@ -2,6 +2,7 @@
 
 #include "AbilityIconFill.h"
 #include "Application.h"
+#include "BarFill.h"
 #include "CameraComponent.h"
 #include "CameraMovement.h"
 #include "Component.h"
@@ -17,7 +18,6 @@
 #include "ResourceMaterial.h"
 #include "ResourceStateMachine.h"
 #include "ResourcesModule.h"
-#include "BarFill.h"
 #include "Scene.h"
 #include "SceneModule.h"
 #include "ScriptComponent.h"
@@ -106,15 +106,6 @@ CuChulainn::CuChulainn(GameObject* parent)
     fields.push_back({"Riastrad on enemy hit", InspectorField::FieldType::Int, &riastradOnHit, 0, 100});
     fields.push_back({"Riastrad on enemy defeated", InspectorField::FieldType::Int, &riastradOnEnemyDeath, 0, 100});
     fields.push_back({"Transform VFX Delay", InspectorField::FieldType::Float, &transformVfxDelay, 0.0f, 20.0f});
-    fields.push_back({"Riastrad VFX burst", InspectorField::FieldType::InputText, &riastradBurstName});
-    fields.push_back({"Riastrad VFX blur", InspectorField::FieldType::InputText, &riastradBlurName});
-    fields.push_back({"Riastrad VFX halo", InspectorField::FieldType::InputText, &riastradHaloName});
-    fields.push_back({"Riastrad VFX sphere", InspectorField::FieldType::InputText, &riastradSphereName});
-    fields.push_back({"Riastrad VFX crack", InspectorField::FieldType::InputText, &riastradCrackName});
-    fields.push_back({"Riastrad VFX waring", InspectorField::FieldType::InputText, &riastradWaringName});
-    fields.push_back({"Riastrad VFX smoke 1", InspectorField::FieldType::InputText, &riastradSmoke1Name});
-    fields.push_back({"Riastrad VFX smoke 2", InspectorField::FieldType::InputText, &riastradSmoke2Name});
-    fields.push_back({"Riastrad VFX smoke 3", InspectorField::FieldType::InputText, &riastradSmoke3Name});
 
     fields.push_back({InspectorField::FieldType::Text, (void*)"VFX"});
     fields.push_back({"Aim shadow object", InspectorField::FieldType::InputText, &aimShadowName});
@@ -205,42 +196,6 @@ bool CuChulainn::Init()
     riastradVfx = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradVfxName);
     if (!riastradVfx) GLOG("[WARNING] No riastrad VFX found for CuChulain")
     else riastradVfx->SetEnabled(false);
-
-    riastradBurst = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradBurstName);
-    if (!riastradBurst) GLOG("[WARNING] No riastrad Burst VFX found for CuChulain")
-    else riastradBurst->SetEnabled(false);
-
-    riastradBlur = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradBlurName);
-    if (!riastradBlur) GLOG("[WARNING] No riastrad Blur VFX found for CuChulain")
-    else riastradBlur->SetEnabled(false);
-
-    riastradHalo = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradHaloName);
-    if (!riastradHalo) GLOG("[WARNING] No riastrad Halo VFX found for CuChulain")
-    else riastradHalo->SetEnabled(false);
-
-    riastradSphere = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradSphereName);
-    if (!riastradSphere) GLOG("[WARNING] No riastrad Sphere VFX found for CuChulain")
-    else riastradSphere->SetEnabled(false);
-
-    riastradCrack = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradCrackName);
-    if (!riastradCrack) GLOG("[WARNING] No riastrad Crack VFX found for CuChulain")
-    else riastradCrack->SetEnabled(false);
-
-    riastradWaring = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradWaringName);
-    if (!riastradWaring) GLOG("[WARNING] No riastrad Warning VFX found for CuChulain")
-    else riastradWaring->SetEnabled(false);
-
-    riastradSmoke1 = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradSmoke1Name);
-    if (!riastradSmoke1) GLOG("[WARNING] No riastrad Smoke 1 VFX found for CuChulain")
-    else riastradSmoke1->SetEnabled(false);
-
-    riastradSmoke2 = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradSmoke2Name);
-    if (!riastradSmoke2) GLOG("[WARNING] No riastrad Smoke 2 VFX found for CuChulain")
-    else riastradSmoke2->SetEnabled(false);
-
-    riastradSmoke3 = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradSmoke3Name);
-    if (!riastradSmoke3) GLOG("[WARNING] No riastrad Smoke 3 VFX found for CuChulain")
-    else riastradSmoke3->SetEnabled(false);
 
     GameObject* riastradBarObj = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByName(riastradBarName);
     if (riastradBarObj)
@@ -389,7 +344,6 @@ void CuChulainn::OnDamageTaken(int amount)
 void CuChulainn::OnHealed(int amount)
 {
     // TODO: play CuChulainn recover sound
-    // TODO: update health bar
     if (healthBar) healthBar->SetFillAmount(static_cast<float>(currentHealth) / static_cast<float>(maxHealth));
 }
 
@@ -1001,10 +955,6 @@ void CuChulainn::PerformAttack()
 
 void CuChulainn::Attack(float deltaTime)
 {
-    // TODO: play basicAttack sound
-
-    // GLOG("ATTACK");
-
     if (state == CharacterStates::AIM && camera)
     {
         if (meleeTrailObject) meleeTrailObject->SetEnabled(true);
@@ -1161,7 +1111,6 @@ bool CuChulainn::TakeMushroom()
         mushrooms += 1;
         state      = CharacterStates::TAKE_MUSHROOM;
         taken      = true;
-        // TODO: take mushrooms anim (maybe not animation), vfx etc
     }
 
     desiredTakeMushroom = false;
@@ -1307,56 +1256,12 @@ void CuChulainn::EnableRiastradVfx()
         mat->SetDiffColor(newColor);
     }
 
-    // TODO: Adjust timings for every specific effect
     if (riastradVfx)
     {
         riastradVfx->SetEnabled(true);
+        riastradVfx->SetLocalPosition(parent->GetLocalTransform().TranslatePart());
         riastradVfx->GetComponent<AnimationComponent*>()->OnPlay(true);
     }
-    // if (riastradSmoke1)
-    //{
-    //     riastradSmoke1->SetEnabled(true);
-    //     riastradSmoke1->GetComponent<MeshComponent*>()->SetEnabled(false);
-    //     //riastradSmoke1->GetComponent<ShaderScriptComponent*>()->GetScriptByType<MovingUVTransparent>()->Reset();
-    // }
-    // if (riastradSmoke2)
-    //{
-    //     riastradSmoke2->SetEnabled(true);
-    //     riastradSmoke2->GetComponent<MeshComponent*>()->SetEnabled(false);
-    //     //riastradSmoke2->GetComponent<ShaderScriptComponent*>()->GetScriptByType<MovingUVTransparent>()->Reset();
-    // }
-    // if (riastradSmoke3)
-    //{
-    //     riastradSmoke3->SetEnabled(true);
-    //     riastradSmoke3->GetComponent<MeshComponent*>()->SetEnabled(false);
-    //     //riastradSmoke3->GetComponent<ShaderScriptComponent*>()->GetScriptByType<MovingUVTransparent>()->Reset();
-    // }
-    // if (riastradSphere)
-    //{
-    //     riastradSphere->SetEnabled(true);
-    //    //riastradSphere->GetComponent<MeshComponent*>()->SetEnabled(false);
-    //    //riastradSphere->GetComponent<ShaderScriptComponent*>()->GetScriptByType<MovingUVTransparent>()->Reset();
-    // }
-    // if (riastradHalo)
-    //{
-    //     riastradHalo->SetEnabled(true);
-    //     //riastradHalo->GetComponent<MeshComponent*>()->SetEnabled(false);
-    //     //riastradHalo->GetComponent<ShaderScriptComponent*>()->GetScriptByType<MovingUVTransparent>()->Reset();
-    // }
-    // if (riastradCrack) riastradCrack->SetEnabled(true);
-    // if (riastradWaring) riastradWaring->SetEnabled(true);
-    // if (riastradBurst)
-    //{
-    //     riastradBurst->SetEnabled(true);
-    //     //riastradBurst->GetComponent<MeshComponent*>()->SetEnabled(false);
-    //     //riastradBurst->GetComponent<ShaderScriptComponent*>()->GetScriptByType<MovingUVTransparent>()->Reset();
-    // }
-    // if (riastradBlur)
-    //{
-    //     riastradBlur->SetEnabled(true);
-    //     //riastradBlur->GetComponent<MeshComponent*>()->SetEnabled(false);
-    //     //riastradBlur->GetComponent<ShaderScriptComponent*>()->GetScriptByType<MovingUVTransparent>()->Reset();
-    // }
 }
 
 void CuChulainn::AddRiastrad(int amount)
