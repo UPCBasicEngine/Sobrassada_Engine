63d970763fb1d85acd7978b51d58171e4457c51a
caspi99 <65339956+caspi99@users.noreply.github.com>
2025-08-27 19:51:37 +0200

    THO-783 water spouts trap final version 2 (#352)
    * Video Component
    
    * Ffmpeg lib
    
    * Added dll
    
    * Solved merge
    
    * video
    
    * Video in gbuffer
    
    * Video done
    
    * Cleaning
    
    * spouts
    
    * Added timer
    
    * SpoutsDecalRune
    
    * Add audio logic
    
    * Fix player charge attacks not working
    
    ---------
    
    Co-authored-by: PolCurto <115176650+PolCurto@users.noreply.github.com>

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Spouts.h
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Spouts.h
@@ -5,6 +5,8 @@
 class ShaderScriptComponent;
 class MeshComponent;
 class SphereColliderComponent;
+class ParticleSystemComponent;
+class AudioSourceComponent;
 
 class Spouts : public Script
 {
@@ -23,16 +25,19 @@ class Spouts : public Script
     int GetDamage() { return damage; }
 
   private:
+    bool enableRune                         = false;
     float activationRange                   = 10.0f;
     int damage                              = 1;
     float chargingDuration                  = 1.0f;
-    float chargingTimer                     = 0.0f;
+    float chargingTimer                     = 0.01f;
+    float spoutWaterTimer                   = 0.01f;
     float rotationSpeedWhiteWaves           = 90.0f;
     float minScaleTornado                   = 0.1f;
     float maxScaleTornado                   = 12.0f;
     float initialScaleTornado               = 9.0f;
     float rotationSpeedTornado              = 90.0f;
     float rotationSpeedBlueWaves            = 90.0f;
+    float explosionDuration                 = 0.01f;
 
     GameObject* character                   = nullptr;
     GameObject* whiteWaves                  = nullptr;
@@ -40,9 +45,21 @@ class Spouts : public Script
     GameObject* blueWaves                   = nullptr;
     GameObject* explosion                   = nullptr;
     GameObject* waterMesh                   = nullptr;
-    ShaderScriptComponent* shaderScript     = nullptr;
-    MeshComponent* shaderMesh               = nullptr;
-    SphereColliderComponent* damageCollider = nullptr;
+    GameObject* particleGO                  = nullptr;
+    GameObject* rune                        = nullptr;
 
+    ShaderScriptComponent* whiteWavesScript = nullptr;
+    MeshComponent* shaderwhiteWavesMesh     = nullptr;
+
+    ShaderScriptComponent* shaderScript     = nullptr;
+    MeshComponent* shaderWaterMesh          = nullptr;
+
+    ShaderScriptComponent* explosionScript  = nullptr;
+    MeshComponent* shaderExplosionMesh      = nullptr;
+
+    SphereColliderComponent* damageCollider = nullptr;
+    ParticleSystemComponent* particles      = nullptr;
+
+    AudioSourceComponent* audio             = nullptr;
     ACTIVATION_STATE activationState        = SLEEPING;
 };
