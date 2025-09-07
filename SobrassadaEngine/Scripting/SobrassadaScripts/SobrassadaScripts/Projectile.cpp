653e26e363c5e8dbbddcd8c9963a1ff314243448
Marc López <88272783+MarcLopezS@users.noreply.github.com>
2025-09-07 14:59:13 +0200

    Proyectile first version (#361)
    * updated method shoot and move
    
    * enable/ dissable weapon when shooting
    
    * added string mesh weapon
    
    * fixed variables
    
    * moved initialization to init method
    
    * fixed bug frozen proyectile

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Projectile.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Projectile.cpp
@@ -60,14 +60,19 @@ void Projectile::Shoot(const float3& origin, const float3& direction)
 {
    
     startPos        = origin;
-    this->direction = direction;
+    this->direction = direction.Normalized();
     frames          = 0;
+    if (collider) collider->SetEnabled(false);
     
     // Rotate spear object
     const float3 scale       = parent->GetLocalTransform().ExtractScale();
-    const Quat rotation      = Quat::LookAt(float3::unitZ, direction, float3::unitY, float3::unitY);
+    const Quat rotation      = Quat::LookAt(float3::unitZ, this->direction, float3::unitY, float3::unitY);
     const float4x4 transform = float4x4::FromTRS(origin, rotation, scale);
-    parent->SetLocalTransform(transform);
+    
+    const float4x4 parentWS  = parent->GetParentGlobalTransform();
+    const float4x4 localTRS  = parentWS.Inverted() * transform;
+    
+    parent->SetLocalTransform(localTRS);
 
     parent->SetEnabledRecursive(true);
 }
@@ -100,13 +105,17 @@ void Projectile::Move(float deltaTime)
     frames += 1;
     if (frames > 20 && collider && !collider->GetEnabled()) collider->SetEnabled(true);
 
-    float3 currentPos  = parent->GetPosition();
-    currentPos        += direction * speed * deltaTime;
-    parent->SetLocalPosition(currentPos);
+    //float3 currentPos  = parent->GetPosition();
+    //currentPos        += direction * speed * deltaTime;
+    
 
-    const float3 worldPos = parent->GetGlobalTransform().TranslatePart();
+    const float3 worldPos = parent->GetGlobalTransform().TranslatePart() + direction * speed * deltaTime;
+    const float4x4 parentWS = parent->GetParentGlobalTransform();
+    const float3 nextLocal  = (parentWS.Inverted() * float4(worldPos, 1.0f)).xyz();
 
-    if (currentPos.Distance(startPos) > range || !IsInsideCameraView(worldPos, 0.11f))
+    parent->SetLocalPosition(nextLocal);
+
+    if (worldPos.Distance(startPos) > range || !IsInsideCameraView(worldPos, 0.0f))
     {
         parent->SetEnabled(false);
     }
