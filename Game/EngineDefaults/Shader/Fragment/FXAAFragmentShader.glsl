6042af95a303e31c1a5c4428ccb4c2596fd55eca
PolCurto <115176650+PolCurto@users.noreply.github.com>
2025-09-18 12:52:27 +0200

    Shader refactor and fixes

--- Game/EngineDefaults/Shader/Fragment/FXAAFragmentShader.glsl
+++ Game/EngineDefaults/Shader/Fragment/FXAAFragmentShader.glsl
@@ -11,6 +11,28 @@ layout(location=3) uniform bool enableFXAA;
 
 out vec4 fragColor;
 
+struct LuminanceData
+{
+    float middle;
+    float north;
+    float east;
+    float south;
+    float west;
+    float northEast;
+    float southEast;
+    float southWest;
+    float northWest;
+    float contrast;
+};
+
+struct EdgeData 
+{
+    bool isHorizontal;
+    vec2 pixelStep;
+    float oppositeLuminance;
+    float gradient;
+};
+
 float luma(const vec4 color)
 {
     return color.g;
@@ -39,84 +61,118 @@ float getMin(float values[5])
 vec2 ComputeBlendDirection(float middle, float neighbour, float oppositeNeighbour)
 {
     vec2 pixelStep;
-    float pGradient = abs(neighbour - middle);
-    float nGradient = abs(oppositeNeighbour - middle);
-    if (pGradient >= nGradient)
-    {
-        pixelStep = vec2(0.0f, 1.0f / textureSize(lastDrawTexture, 0).y);
-    }
-    else
-    {
-        pixelStep = vec2(0.0f, -1.0f / textureSize(lastDrawTexture, 0).y);
-    }
+    
     return pixelStep;
 }
 
-vec3 ApplyFXAA(float middle, float north, float east, float south, float west, float contrast) 
+float ComputePixelBlendFactor(LuminanceData l)
 {
-    // Pixel blend factor
-    float northEast = luma(textureOffset(lastDrawTexture, uv0, ivec2(1, 1)));
-    float southEast = luma(textureOffset(lastDrawTexture, uv0, ivec2(1, -1)));
-    float southWest = luma(textureOffset(lastDrawTexture, uv0, ivec2(-1, -1)));
-    float northWest = luma(textureOffset(lastDrawTexture, uv0, ivec2(-1, 1)));
-
-    float average = 2.0f * (north + east + south + west) + northEast + southEast + southWest + northWest;
+    float average = 2.0f * (l.north + l.east + l.south + l.west) + l.northEast + l.southEast + l.southWest + l.northWest;
     average /= 12.0f;
 
-    float pixelBlendFactor = abs(middle - average) / contrast;
+    float pixelBlendFactor = abs(l.middle - average) / l.contrast;
     pixelBlendFactor = smoothstep(0.0f, 1.0f, clamp(pixelBlendFactor, 0.0f, 1.0f));
-    pixelBlendFactor *= pixelBlendFactor;
+    return pixelBlendFactor * pixelBlendFactor;
+}
 
+EdgeData ComputeEdgeDirection(LuminanceData l)
+{
+    EdgeData e;
 
     // Edge blend direction
-    float verticalEdge = abs((0.25 * northWest) + (-0.5 * north) + (0.25 * northEast)) +        
-                        abs((0.50 * west ) + (-1.0 * middle) + (0.50 * east )) + 
-                        abs((0.25 * southWest) + (-0.5 * south) + (0.25 * southEast)); 
+    float verticalEdge = abs((0.25 * l.northWest) + (-0.5 * l.north) + (0.25 * l.northEast)) +        
+                        abs((0.50 * l.west ) + (-1.0 * l.middle) + (0.50 * l.east )) + 
+                        abs((0.25 * l.southWest) + (-0.5 * l.south) + (0.25 * l.southEast)); 
                         
-    float horizontalEdge = abs((0.25 * northWest) + (-0.5 * west) + (0.25 * southWest)) + 
-                        abs((0.50 * north ) + (-1.0 * middle) + (0.50 * south)) + 
-                        abs((0.25 * northEast) + (-0.5 * east) + (0.25 * southEast));
+    float horizontalEdge = abs((0.25 * l.northWest) + (-0.5 * l.west) + (0.25 * l.southWest)) + 
+                        abs((0.50 * l.north ) + (-1.0 * l.middle) + (0.50 * l.south)) + 
+                        abs((0.25 * l.northEast) + (-0.5 * l.east) + (0.25 * l.southEast));
     
-    bool isHorizontal = horizontalEdge >= verticalEdge; 
+    e.isHorizontal = horizontalEdge >= verticalEdge; 
 
-    float neighbour = isHorizontal ? north : east;
-    float oppositeNeighbour = isHorizontal ? east : west;
+    // Compute edge direction
+    float neighbour = e.isHorizontal ? l.north : l.east;
+    float oppositeNeighbour = e.isHorizontal ? l.south : l.west;
 
-    vec2 pixelStep = ComputeBlendDirection(middle, neighbour, oppositeNeighbour);   
-    return texture(lastDrawTexture, uv0 + pixelStep * pixelBlendFactor).rgb;
+    float pGradient = abs(neighbour - l.middle);
+    float nGradient = abs(oppositeNeighbour - l.middle);
+    if (pGradient >= nGradient)
+    {
+        e.pixelStep = e.isHorizontal ? vec2(0.0f, 1.0f / textureSize(lastDrawTexture, 0).y) : vec2(1.0f / textureSize(lastDrawTexture, 0).x, 0.0f);
+        e.oppositeLuminance = neighbour;
+        e.gradient = pGradient;
+    }
+    else
+    {
+        e.pixelStep = e.isHorizontal ? vec2(0.0f, -1.0f / textureSize(lastDrawTexture, 0).y) : vec2(-1.0f / textureSize(lastDrawTexture, 0).x, 0.0f);
+        e.oppositeLuminance = oppositeNeighbour;
+        e.gradient = nGradient;
+    }
+
+    return e;
+}
+
+float ComputeEdgeBlendFactor(LuminanceData l, EdgeData e)
+{
+    vec2 currentUv = uv0;
+
+    currentUv += e.pixelStep * 0.5f;
+
+    return 0;
+}
+
+vec3 ApplyFXAA(LuminanceData l) 
+{
+    float pixelBlendFactor = ComputePixelBlendFactor(l);
+    EdgeData e = ComputeEdgeDirection(l);
+    float edgeBlendFactor = ComputeEdgeBlendFactor(l, e);
+
+    if (showBorders)
+    {
+        vec3 color = e.isHorizontal ? vec3(0.0f, 1.0f, 0.0f) : vec3(1.0f, 0.0f, 0.0f);
+        return color;
+    }
+    else
+    {
+        return texture(lastDrawTexture, uv0 + e.pixelStep * pixelBlendFactor).rgb;
+    }
 }
 
 void main()
 {
-    if (!enableFXAA)  // If disabkle, just pass-through. Needed for drawing to screen framebuffer anyway
+    if (!enableFXAA)  // If disable, just pass-through. Needed for drawing to screen framebuffer anyway
     {
         fragColor = texture(lastDrawTexture, uv0);
         return;
     }
 
-    float middle = luma(texture(lastDrawTexture, uv0));
-    float north = luma(textureOffset(lastDrawTexture, uv0, ivec2(0, 1)));
-    float east = luma(textureOffset(lastDrawTexture, uv0, ivec2(1, 0)));
-    float south = luma(textureOffset(lastDrawTexture, uv0, ivec2(0, -1)));
-    float west = luma(textureOffset(lastDrawTexture, uv0, ivec2(-1, 0)));
+    LuminanceData l;
+
+    // Immediate neighbours
+    l.middle = luma(texture(lastDrawTexture, uv0));
+    l.north = luma(textureOffset(lastDrawTexture, uv0, ivec2(0, 1)));
+    l.east = luma(textureOffset(lastDrawTexture, uv0, ivec2(1, 0)));
+    l.south = luma(textureOffset(lastDrawTexture, uv0, ivec2(0, -1)));
+    l.west = luma(textureOffset(lastDrawTexture, uv0, ivec2(-1, 0)));
+
+    // Diagonal neighbours
+    l.northEast = luma(textureOffset(lastDrawTexture, uv0, ivec2(1, 1)));
+    l.southEast = luma(textureOffset(lastDrawTexture, uv0, ivec2(1, -1)));
+    l.southWest = luma(textureOffset(lastDrawTexture, uv0, ivec2(-1, -1)));
+    l.northWest = luma(textureOffset(lastDrawTexture, uv0, ivec2(-1, 1)));
     
-    float luminance[5] = float[5](middle, north, east, south, west);
+    float luminance[5] = float[5](l.middle, l.north, l.east, l.south, l.west);
     float maxValue = getMax(luminance);
     float minValue = getMin(luminance);
-    float contrast = maxValue - minValue;
+    l.contrast = maxValue - minValue;
 
-    if(contrast < max(localThreshold * maxValue, globalThreshold))
+    if(l.contrast < max(localThreshold * maxValue, globalThreshold))
     {
         fragColor = texture(lastDrawTexture, uv0);
+        //fragColor = vec4(vec3(0.0f), 1.0f);
     }
     else
     {
-        if (showBorders) 
-        {
-            fragColor = vec4(0.0, 0.0, 0.0, 1.0);
-        } else 
-        {
-            fragColor = vec4(ApplyFXAA(middle, north, east, south, west, contrast), 1.0f);
-        }
+        fragColor = vec4(ApplyFXAA(l), 1.0f);
     } 
 }
