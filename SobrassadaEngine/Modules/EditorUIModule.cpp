e1bbb887a2def7b94e7463a1f9e2122afdd2c21f
IDarkgenesis <byidarkgenesis@gmail.com>
2025-10-03 00:57:16 +0200

    More range to options

--- SobrassadaEngine/Modules/EditorUIModule.cpp
+++ SobrassadaEngine/Modules/EditorUIModule.cpp
@@ -558,15 +558,15 @@ void EditorUIModule::FogConfig(bool& fogConfig)
 
     ImGui::Begin("Fog Config", &fogConfig, ImGuiWindowFlags_None);
 
-    if (ImGui::DragFloat("Step Size", &renderPass->stepSize, 0.05f, 0.1f, 1.f))
+    if (ImGui::DragFloat("Step Size", &renderPass->stepSize, 0.05f, 0.4f, 1.f))
     {
-        if (renderPass->stepSize == 0) renderPass->stepSize = 0.1f;
+        if (renderPass->stepSize < 0.4f) renderPass->stepSize = 0.4f;
     }
     ImGui::DragFloat("Fog intensity", &renderPass->fogIntensity, 0.01f, 0.0f, 1.f);
     ImGui::DragFloat("Noise ammount", &renderPass->noiseAmmount, 0.01f, 0.0f, 1.f);
     ImGui::DragFloat("Extinction Coefficient", &renderPass->extinctionCoefficient, 0.01f, 0.01f, 1.f);
     ImGui::DragFloat("Anisotropy", &renderPass->anisotropy, 0.01f, -0.99f, 0.99f);
-    if (ImGui::DragInt("Blurr passes", &renderPass->blurrPasses, 1, 0, 20))
+    if (ImGui::DragInt("Blurr passes", &renderPass->blurrPasses, 1, 0, 250))
     {
         if (renderPass->blurrPasses < 0) renderPass->blurrPasses = 0;
     }
