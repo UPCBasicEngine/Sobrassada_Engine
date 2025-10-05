8c8b73ddfaaf8d9a27d3aab6b5df0a6c7032ef67
Joan Martí Olivart <79135823+jmartiolivart@users.noreply.github.com>
2025-10-05 20:41:09 +0200

    Fireballtrap-revision-THO-1003 (#404)
    * Added animations
    
    * Update FireballTrap.cpp
    
    * Animation showing WIP
    
    * Animations not playing WIP
    
    * Animations starting to work and play correctly
    
    Still have some issues but WIP good
    
    * Three animation working
    
    The animations now play and can be placed in different positions.
    The mini animation replicates but still to vague to see it (WIP)
    
    * Added multiple animations mini
    
    - Added rotation mini
    - Added new VFX
    - Added new sound for fireball
    
    * Add reset VFX
    
    * Update FireballTrap.cpp
    
    * Change symbol to minis
    
    * update size minis
    
    * Fix multiple firetraps issue
    
    * add working peace particle system
    
    * clean header
    
    * adjusting timings
    
    * Small indicators appearing before
    
    * added the shadow shader correctly
    
    * Update FireballTrap.cpp
    
    * Fixed blackstain
    
    * Added value for particle system
    
    * Fixed comments

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/MiniFireball.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/MiniFireball.cpp
@@ -1,15 +1,28 @@
 ﻿#include "pch.h"
 
 #include "MiniFireball.h"
+#include "Character.h"
 #include "GameObject.h"
-#include "ScriptComponent.h" 
-#include "Character.h"  
-
+#include "ScriptComponent.h"
 #include <algorithm>
 
 bool MiniFireball::Init()
 {
     lifeTimer = 0.f;
+
+    // Apply initial random rotation if specified
+    if (initialRotation.x != 0.f || initialRotation.y != 0.f || initialRotation.z != 0.f)
+    {
+        currentRotation           = initialRotation;
+
+        // Apply initial rotation by multiplying current transform
+        float4x4 currentTransform = parent->GetLocalTransform();
+        float4x4 rotX             = float4x4::RotateX(initialRotation.x * DEGREE_RAD_CONV);
+        float4x4 rotY             = float4x4::RotateY(initialRotation.y * DEGREE_RAD_CONV);
+        float4x4 rotZ             = float4x4::RotateZ(initialRotation.z * DEGREE_RAD_CONV);
+
+        parent->SetLocalTransform(currentTransform * rotX * rotY * rotZ);
+    }
 
     if (!parent->GetChildren().empty())
     {
@@ -23,26 +36,36 @@ bool MiniFireball::Init()
     return true;
 }
 
-
 void MiniFireball::Update(float deltaTime)
 {
     lifeTimer += deltaTime;
-    if (lifeTimer >= life) parent->SetEnabled(false);
+    if (lifeTimer >= life)
+    {
+        parent->SetEnabled(false);
+        return;
+    }
+
+    // Apply continuous rotation incrementally
+    if (rotationSpeed.x != 0.f || rotationSpeed.y != 0.f || rotationSpeed.z != 0.f)
+    {
+        float3 deltaRot = rotationSpeed * deltaTime * DEGREE_RAD_CONV;
+
+        // Apply rotation as incremental transforms
+        float4x4 spin   = float4x4::RotateX(deltaRot.x) * float4x4::RotateY(deltaRot.y) * float4x4::RotateZ(deltaRot.z);
+
+        // Multiply current transform by the rotation
+        parent->SetLocalTransform(parent->GetLocalTransform() * spin);
+    }
 
     if (shadow && parent->IsEnabled())
     {
-        //local position respect to the firetrap
+        // Shadow movement
         float3 pLocal      = parent->GetLocalTransform().TranslatePart();
-
-        // shadow movment
         float3 localOffset = float3(0.f, -pLocal.y, 0.f);
-        float3 scale       = baseScale;
-        float4x4 tf        = float4x4::FromTRS(localOffset, float3x3::identity, scale);
+        float4x4 tf        = float4x4::FromTRS(localOffset, float3x3::identity, baseScale);
         shadow->SetLocalTransform(tf);
     }
 }
-
-
 
 void MiniFireball::OnCollision(GameObject* other, const float3 normal, ColliderLayer layer)
 {
--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/MiniFireball.h
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/MiniFireball.h
@@ -5,16 +5,22 @@ class MiniFireball : public Script
 {
   public:
     MiniFireball(GameObject* parent) : Script(parent) {}
-
-    int damage = 1;   
-    float life = 3.f; 
+    int damage = 1;
+    float life = 3.f;
 
     bool Init() override;
     void Update(float dt) override;
     void OnCollision(GameObject* other, const float3 normal, ColliderLayer layer) override;
 
+    // Set initial random rotation from FireballTrap
+    void SetInitialRotation(const float3& rotation) { initialRotation = rotation; }
+    void SetRotationSpeed(const float3& speed) { rotationSpeed = speed; }
+
   private:
-    float lifeTimer = 0.f;
-    GameObject* shadow = nullptr;
-    float3 baseScale   = float3::one;
+    float lifeTimer        = 0.f;
+    GameObject* shadow     = nullptr;
+    float3 baseScale       = float3::one;
+    float3 rotationSpeed   = float3(90.f, 120.f, 60.f); // degrees per second
+    float3 currentRotation = float3::zero;
+    float3 initialRotation = float3::zero;
 };
