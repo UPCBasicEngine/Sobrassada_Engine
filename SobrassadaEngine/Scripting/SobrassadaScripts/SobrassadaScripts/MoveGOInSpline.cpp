17f917c35f7c72f7a856cd7f9563878ceacefe27
LukasGanter <44579580+LukasGanter@users.noreply.github.com>
2025-10-17 20:02:28 +0200

    Name display and character highlight THO-1057 (#449)
    * Script for displaying a child go for a set amount of time
    
    * Added name display to highlight zoom
    
    * Added functional player highlight zoom
    
    * Formatting, filters, optional delay for spawn ui, character zoom changes
    
    * relocate pch.h
    
    ---------
    
    Co-authored-by: Marc López <88272783+MarcLopezS@users.noreply.github.com>

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/MoveGOInSpline.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/MoveGOInSpline.cpp
@@ -1,11 +1,11 @@
 #include "pch.h"
 #include "MoveGOInSpline.h"
 
+
 #include "Application.h"
 #include "GameObject.h"
 #include "SceneModule.h"
 #include "Standalone/SplineComponent.h"
-
 
 MoveGOInSpline::MoveGOInSpline(GameObject* parent) : Script(parent)
 {
@@ -16,9 +16,9 @@ MoveGOInSpline::MoveGOInSpline(GameObject* parent) : Script(parent)
 bool MoveGOInSpline::Init()
 {
     spline = FindSpline();
-    
+
     if (!spline) GLOG("[MoveGOInSpline] GameObject '%s' did not found spline.", parent->GetName().c_str());
-    
+
     return true;
 }
 
@@ -31,25 +31,25 @@ void MoveGOInSpline::Update(float deltaTime)
     }
     if (speed <= 0.0f) return;
 
-    float segSpeed = spline->EvaluateSpeed(t) * speed;
+    float segSpeed     = spline->EvaluateSpeed(t) * speed;
 
     const int ptCount  = static_cast<int>(spline->GetNumPoints());
     const bool isLoop  = spline->IsLoop();
     const int segCount = isLoop ? ptCount : ptCount - 1;
 
     float segFloat     = t * segCount;
-    int idxStart           = static_cast<int>(floorf(segFloat));
-    
+    int idxStart       = static_cast<int>(floorf(segFloat));
+
     if (isLoop) idxStart %= ptCount;
 
     int idxEnd    = isLoop ? (idxStart + 1) % ptCount : (std::min)(idxStart + 1, ptCount - 1);
-    
+
     float3 pStart = spline->GetPointWorld(idxStart);
     float3 pEnd   = spline->GetPointWorld(idxEnd);
     float segLen  = (pEnd - pStart).Length();
 
     if (segLen < 0.0001f) segLen = 0.0001f;
-    
+
     float deltaT = (segSpeed * deltaTime) / (segLen * segCount);
 
     if (pingPong && !isLoop)
@@ -71,26 +71,33 @@ void MoveGOInSpline::Update(float deltaTime)
     else
     {
         t += deltaT;
-        if (t > 1.f) t -= 1.f;
-        else if (t < 0.f) t += 1.f;
+        if (t > 1.f)
+        {
+            t -= 1.f;
+            loopCounter++;
+        }
+        else if (t < 0.f)
+        {
+            t += 1.f;
+            loopCounter++;
+        }
     }
 
     float3 worldPos;
     Quat worldRot;
     spline->EvaluateTransform(t, worldPos, worldRot);
-    
+
     const float4x4& parentGlob = parent->GetParentGlobalTransform();
 
     float4x4 worldM            = float4x4::FromTRS(worldPos, worldRot.ToFloat4x4(), float3::one);
-    
+
     float4x4 localM            = parentGlob.Inverted() * worldM;
     parent->SetLocalTransform(localM);
-
 }
 
 SplineComponent* MoveGOInSpline::FindSpline()
 {
-    UID parentUID = parent->GetParent();
+    UID parentUID      = parent->GetParent();
     GameObject* parent = AppEngine->GetSceneModule()->GetScene()->GetGameObjectByUID(parentUID);
 
     if (!parent) return nullptr;
@@ -110,8 +117,6 @@ SplineComponent* MoveGOInSpline::FindSpline()
             return spline;
         }
     }
-    
+
     return nullptr;
 }
-
-
