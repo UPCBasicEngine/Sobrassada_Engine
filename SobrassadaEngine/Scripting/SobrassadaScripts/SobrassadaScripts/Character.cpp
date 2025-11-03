d3adb5693e3f386f9671268606dc53e3e6450ef7
Marc Casanova <13cvs6a@gmail.com>
2025-11-03 19:37:52 +0100

    inspector mesh2 name input text deleted for soldier

--- SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Character.cpp
+++ SobrassadaEngine/Scripting/SobrassadaScripts/SobrassadaScripts/Character.cpp
@@ -62,10 +62,9 @@ Character::Character(
         fields.push_back({"AI Attack Range", InspectorField::FieldType::Float, &rangeAIAttack, 0.0f, 25.0f});
         fields.push_back({"AI Max Detection Range", InspectorField::FieldType::Float, &maxDetectionRange, 0.0f, 30.0f});
         fields.push_back({"Player search duration", InspectorField::FieldType::Float, &searchDuration, 0.0f, 10.0f});
-        if (type == CharacterType::Boss || type == CharacterType::Soldier)
-            fields.push_back({"Mesh 2 name", InspectorField::FieldType::InputText, &mesh2Name});
         if (type == CharacterType::Boss)
         {
+            fields.push_back({"Mesh 2 name", InspectorField::FieldType::InputText, &mesh2Name});
             fields.push_back({"Mesh 3 name", InspectorField::FieldType::InputText, &mesh3Name});
             fields.push_back({"Mesh 4 name", InspectorField::FieldType::InputText, &mesh4Name});
         }
