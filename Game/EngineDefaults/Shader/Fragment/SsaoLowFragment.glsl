8465729324ef9c8be6bb31e03b408df630cdb8a9
caspi99 <andreu.c.j99@gmail.com>
2025-11-05 13:54:27 +0100

    SSAO better

--- Game/EngineDefaults/Shader/Fragment/SsaoLowFragment.glsl
+++ Game/EngineDefaults/Shader/Fragment/SsaoLowFragment.glsl
@@ -23,7 +23,7 @@ out vec4 result;
 
 mat3 createTangentSpace(const vec3 normal, const vec3 up)
 {
-   const vec3 tangent   = normalize(up-normal*dot(normal, up));
+   const vec3 tangent   = normalize(up-normal * dot(normal, up));
    const vec3 bitangent = normalize(cross(tangent, normal)); 
    return mat3(bitangent, tangent, normal);
 }
@@ -33,34 +33,30 @@ vec3 getRandomTangent() {
    return normalize(texture(noiseTexture, uv0 * noiseScale).xyz);
 }
 
-
-float getSceneDepthAtSamplePos(in vec3 samplePos)
-{
-    const vec4 clipSpace = projMatrix * vec4(samplePos, 1.0);
-    const vec2 sampleUV = (clipSpace.xy/clipSpace.w)*0.5+0.5;
-
-    return (viewMatrix*texture(gPositions, sampleUV)).z;
-}
-
 void main()
- {
-    const vec3 position     = (viewMatrix*vec4(texture(gPositions, uv0).xyz, 1.0)).xyz;
-    const vec3 normal       = mat3(viewMatrix)*normalize(texture(gNormals, uv0).xyz);
+{
+    const vec3 position     = (viewMatrix * vec4(texture(gPositions, uv0).xyz, 1.0)).xyz;
+    const vec3 normal       = mat3(viewMatrix) * normalize(texture(gNormals, uv0).xyz);
     const mat3 tangentSpace = createTangentSpace(normal, getRandomTangent());
     int occlusion     = 0;
 
     for(int i=0; i< KERNEL_SIZE; ++i) 
     {
-        vec3 samplePos = position+tangentSpace*kernel_samples[i];
-        float sampleDepth = getSceneDepthAtSamplePos(samplePos);
-        if(getSceneDepthAtSamplePos(samplePos)+bias > samplePos.z && abs(sampleDepth-position.z) < range)
-	{
-	 ++occlusion;
-	}
+        vec3 samplePos = position + tangentSpace * kernel_samples[i];
+
+        const vec4 clipSpace = projMatrix * vec4(samplePos, 1.0);
+        const vec2 sampleUV = (clipSpace.xy/clipSpace.w) * 0.5 + 0.5;
+
+        if(sampleUV.x < 0.0 || sampleUV.x > 1.0 || sampleUV.y < 0.0 || sampleUV.y > 1.0) continue;
+        float sampleDepth = (viewMatrix * texture(gPositions, sampleUV)).z;
+
+        if(sampleDepth + bias > samplePos.z && abs(sampleDepth - position.z) < range)
+        {
+            ++occlusion;
+        }
     }
 
     const float ao = 1.0 - float(occlusion) / float(KERNEL_SIZE);
     result = vec4(vec3(ao), 1.0);
-
- }
+}
 
