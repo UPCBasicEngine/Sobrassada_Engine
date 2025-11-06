e98b0a04d0884dc5512c0ba2328dd26b9eea7df4
IDarkgenesis <byidarkgenesis@gmail.com>
2025-11-06 20:24:23 +0100

    Yes

--- SobrassadaEngine/Scene/Scene.h
+++ SobrassadaEngine/Scene/Scene.h
@@ -169,6 +169,8 @@ class SOBRASADA_API_ENGINE Scene
     //void RemoveTransformUpdatedGameObject(GameObject* gameObject) { transformUpdatedGameObjects.erase(gameObject); };
     //void ClearTransformTransformUpdatedGameObjects() { transformUpdatedGameObjects.clear(); };
 
+    void SetResetStaticObjects();
+
   private:
     void CreateStaticSpatialDataStruct();
     void CreateDynamicSpatialDataStruct();
